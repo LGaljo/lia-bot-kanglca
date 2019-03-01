a7ddee36a8d30edc041a46dbe5e77543f3618204
LGaljo <luka.galjot@gmail.com>
2019-03-01 21:00:22 +0100

    Save game

--- src/MyBot.java
+++ src/MyBot.java
@@ -53,7 +53,7 @@ public class MyBot implements Bot {
 
         // If from all of your units less than 60% are workers
         // and you have enough resources, then create a new worker.
-        if (numberOfWorkers / (float) state.units.length < 0.5f && numberOfWorkers < 7 && state.time < 120) {
+        if (numberOfWorkers / (float) state.units.length < 0.5f && numberOfWorkers < 7 && state.time < 125) {
             if (state.resources >= Constants.WORKER_PRICE) {
                 api.spawnUnit(UnitType.WORKER);
             }
@@ -101,7 +101,7 @@ public class MyBot implements Bot {
     private static boolean teamfire(GameState state, Api api, UnitData unit) {
         for (int i = 0; i < state.units.length; i++) {
             UnitData teammate = state.units[i];
-            if (unit.id != teammate.id && distancePointLine(unit, teammate) < 1.0) {
+            if (unit.id != teammate.id && distancePointLine(unit, teammate) < 0.7f) {
                 return true;
             }
         }
@@ -121,56 +121,6 @@ public class MyBot implements Bot {
 
         return (float) (Math.abs((x3 - x1) * (y1 - y2) - (x1 - x2) * (y3 - y1)) /
                 Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2)));
-    }
-/*
-    private static float estimate(UnitData unit, OpponentInView opponent) {
-        double x = -(unit.x - opponent.x);
-        double y = (unit.y - opponent.y);
-        double v = 32; //m/s hitrost izstrelka
-        double g = 7.2; //m/s hitrost tarce
-        double sqrt = (v*v*v*v) - (g*(g*(x*x) + 2*y*(v*v)));
-        sqrt = Math.sqrt(sqrt);
-        float angleInRadians = (float)Math.atan(((v*v) + sqrt)/(g*x));
-
-        return angleInRadians;
-    }
-*/
-    private static OpponentInView estimateTheFuture(UnitData unit, OpponentInView opponent) {
-        OpponentInView previous = oiv.get(opponent.id);
-
-        if (previous == null) {
-            return opponent;
-        }
-
-        // Razdalja med tvojo enoto in nasprotnikom
-        float dist = MathUtil.distance(opponent.x, opponent.y, unit.x, unit.y);
-
-        // Cas potovanja izstrelka med tvojo enoto in nasprotnikom
-        float time = dist / 32;
-
-        float AxX = previous.x - opponent.x;
-        float AxY = previous.y - opponent.y;
-
-        // Lokaciji pristejem razdaljo odvisno od casa katerega bo metek potreboval do nasprotnika in njegove hitrosti
-        float x = (int) (opponent.x + AxX * time * 7.2) * -1;
-        float y = (int) (opponent.y + AxY * time * 7.2);
-        if (x < 0) {
-            x = 0;
-        }
-        if (y < 0) {
-            y = 0;
-        }
-        if (x > 175) {
-            x = 175;
-        }
-        if (y > 98) {
-            y = 98;
-        }
-
-        opponent.x = x;
-        opponent.y = y;
-
-        return opponent;
     }
 
     private static OpponentInView estimate(UnitData src, OpponentInView dest) {
@@ -216,100 +166,27 @@ public class MyBot implements Bot {
             aimY = 175;
         }
 
-        //System.out.println("bul: x:" + tvx + " y:" + tvy);
-        //System.out.println("moj: x:" + src.x + " y:" + src.y);
-        //System.out.println("now: x:" + dest.x + " y:" + dest.y);
-        //System.out.println("est: x:" + aimX + " y:" + aimY);
-
         dest.x = (float)aimX;
         dest.y = (float)aimY;
 
 
         return dest;
     }
-/*
-    private static double solX = 0.0;
-    private static double solY = 0.0;
 
-    private static OpponentInView estimate(UnitData src, OpponentInView dst) {
-        double enemy_velocity = 7.2;
-        double bullet_velocity = 32.0;
-        double tx = dst.x - src.x;
-        double ty = dst.y - src.y;
-        double tvx = enemy_velocity * Math.cos(dst.orientationAngle);
-        double tvy = enemy_velocity * Math.sin(dst.orientationAngle);
-
-        //float distance = MathUtil.distance(src.x, src.y, dst.x, dst.y);
-
-        // Get quadratic equation components
-        double a = tvx * tvx + tvy * tvy - bullet_velocity * bullet_velocity;
-        double b = 2 * (tvx * tx + tvy * ty);
-        double c = tx * tx + ty * ty;
-
-        // Solve quadratic
-        quad(a, b, c); // See quad(), below
-
-        // Find smallest positive solution
-        if (solX != NaN && solY != NaN) {
-            System.out.println("Got it");
-            double t0 = solX, t1 = solY;
-            double t = Math.min(t0, t1);
-            if (t < 0) {
-                t = Math.max(t0, t1);
-                System.out.println("here t:" + t);
-            }
-            if (t > 0) {
-                dst.x = (float)(dst.x + tvx * t);
-                dst.y = (float)(dst.y + tvy * t);
-            }
-        }
-
-        return dst;
-    }
-
-
-    private static void quad(double a, double b, double c) {
-        solX = 0;
-        solY = 0;
-
-        if (Math.abs(a) < 1e-6) {
-            if (Math.abs(b) < 1e-6) {
-                if (Math.abs(c) < 1e-6) {
-                    solX = 0.0;
-                    solY = 0.0;
-                } else {
-                    solX = NaN;
-                    solY = NaN;
-                }
-            } else {
-                solX = -c / b;
-                solY = -c / b;
-            }
-        } else {
-            double disc = b * b - 4 * a * c;
-            if (disc >= 0) {
-                disc = Math.sqrt(disc);
-                a = 2 * a;
-                solX = (-b - disc) / a;
-                solY = (-b + disc) / a;
-            }
-        }
-    }
-*/
     private static void warriors(GameState state, Api api, UnitData unit) {
         // Get the first opponent that the unit sees.
         if (unit.opponentsInView.length > 0) {
             OpponentInView opponent = unit.opponentsInView[0];
 
-            // TODO: Tukaj dodaj priblizek nasprotnikove lokacije glede na njegovo hitrost in smer
-            OpponentInView estimatedOpponent = estimate(unit, opponent);
+            // Tukaj dodaj priblizek nasprotnikove lokacije glede na njegovo hitrost in smer
+            //OpponentInView estimatedOpponent = estimate(unit, opponent);
 
             // Calculate the aiming angle between units orientation and the opponent. The closer
             // the angle is to 0 the closer is the unit aiming towards the opponent.
-            float aimAngle = MathUtil.angleBetweenUnitAndPoint(unit, estimatedOpponent.x, estimatedOpponent.y);
+            float aimAngle = MathUtil.angleBetweenUnitAndPoint(unit, opponent.x, opponent.y);
 
             // Stop the unit.
-            api.setSpeed(unit.id, Speed.NONE);
+            //api.setSpeed(unit.id, Speed.NONE);
 
             // Based on the aiming angle turn towards the opponent.
             if (aimAngle < 0) {
@@ -359,43 +236,6 @@ public class MyBot implements Bot {
         }
     }
 
-    /*
-        private static int w = 0;
-        private static void firstTimeLoc(Api api, UnitData u) {
-            // Generate new x and y until you get a position on the map
-            // where there is no obstacle. Then move the unit there.
-            int x = 0;
-            int y = 0;
-            switch (w) {
-                case 0:
-                    x = (int) (Math.random() * (Constants.MAP_WIDTH / 2));
-                    y = (int) (Math.random() * (Constants.MAP_HEIGHT / 2));
-                    break;
-                case 1:
-                    x = (int) ((Math.random() * Constants.MAP_WIDTH) + Constants.MAP_WIDTH / 2 - 1);
-                    y = (int) ((Math.random() * Constants.MAP_HEIGHT) + Constants.MAP_HEIGHT / 2 - 1);
-                    break;
-                case 2:
-                    x = (int) (Math.random() * (Constants.MAP_WIDTH));
-                    y = (int) ((Math.random() * (Constants.MAP_HEIGHT)) + Constants.MAP_HEIGHT / 2 - 1);
-                    break;
-                default:
-                    x = (int) (Math.random() * Constants.MAP_WIDTH);
-                    y = (int) (Math.random() * Constants.MAP_HEIGHT);
-                    break;
-            }
-            w++;
-
-            while (true) {
-                // Map is a 2D array of booleans. If map[x][y] equals false it means that
-                // at (x,y) there is no obstacle and we can safely move our unit there.
-                if (!Constants.MAP[x][y]) {
-                    api.navigationStart(u.id, x, y);
-                    break;
-                }
-            }
-        }
-    */
     // Connects your bot to Lia game engine, don't change it.
     public static void main(String[] args) throws Exception {
         NetworkingClient.connectNew(args, new MyBot());
